a4913649c496868e1d5312974af6d23ec2f2897d
Matthew Akrigg <matthewakrigg05@gmail.com>
2024-11-12 10:47:21 +0000

    feat: final getters and setters outside db

--- src/league/Stadium.java
+++ src/league/Stadium.java
@@ -1,5 +1,41 @@
 package league;
 
 public class Stadium {
+	
+	private String stadiumName;
+	private int capacity;
+	private String location;
+	
+	public Stadium(String name, int capacity, String location) {
+		this.setStadiumName(name);
+		this.setCapacity(capacity);
+		this.setLocation(location);
+	}
+
+	
+	// Standard getters and setters
+	public String getStadiumName() {
+		return stadiumName;
+	}
+
+	public void setStadiumName(String stadiumName) {
+		this.stadiumName = stadiumName;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
 }
