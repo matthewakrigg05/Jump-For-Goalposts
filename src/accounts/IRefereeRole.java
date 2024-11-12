37928d60a570586ec2b14bc1b0e24d9ac5c5c8ad
Matthew Akrigg <matthewakrigg05@gmail.com>
2024-11-12 10:12:41 +0000

    feat: adding methods to ref interface

--- src/accounts/IRefereeRole.java
+++ src/accounts/IRefereeRole.java
@@ -1,5 +1,19 @@
 package accounts;
+import league.Match;
+import league.MatchEvent;
 
 public interface IRefereeRole {
+	
+	public static void recordMatchScore(Match match, String score) {
+		
+	}
+	
+	public static void matchToResult(Match match, MatchEvent[] events) {
+		
+	}
+	
+	public static void recordMatchEvents() {
+		
+	}
 
 }
