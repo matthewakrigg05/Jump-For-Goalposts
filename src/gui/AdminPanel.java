c9de3d41d7dcb0deca8f87be963f853fb161ad3a
Matthew Akrigg <matthewakrigg05@gmail.com>
2024-11-20 15:01:09 +0000

    feat: adding panel classes, finished init

---
+++ src/gui/AdminPanel.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.JPanel;
+
+public class AdminPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public AdminPanel() {
+
+	}
+
+}
