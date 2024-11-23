eb52e42bd04383dd2597a14cc340a32a684b15d4
Matthew Akrigg <matthewakrigg05@gmail.com>
2024-11-23 20:33:15 +0000

    feat: changing styling on admin panel

--- src/gui/AdminPanel.java
+++ src/gui/AdminPanel.java
@@ -6,21 +6,26 @@ import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JButton;
+import java.awt.Font;
 
 public class AdminPanel extends JPanel {
 
 	public AdminPanel() {
 		
-		Insets labelFieldInsets = new Insets(0, 0, 5, 5);
+		Insets labelFieldInsets = new Insets(0, 0, 10, 25);
+		Font labelFont = (new Font("Tahoma", Font.PLAIN, 25));
+		int numOfCols = 5;
+		int numOfRows = 11;
+		int colWidth = (getWidth() / numOfCols);
+		int rowHeight = (getHeight() / numOfRows);
 		
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{84, 0, 0, 0, 100, 0};
-		gridBagLayout.rowHeights = new int[]{14, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] {colWidth, colWidth, colWidth, colWidth, colWidth};
+		gridBagLayout.rowHeights = new int[] {rowHeight, rowHeight, rowHeight, rowHeight, rowHeight, rowHeight, rowHeight, rowHeight, rowHeight, rowHeight, rowHeight};
 		setLayout(gridBagLayout);
 		
 		JLabel leagueOptLabel = new JLabel("League Options:");
+		leagueOptLabel.setFont(labelFont);
 		GridBagConstraints gbc_leagueOptLabel = new GridBagConstraints();
 		gbc_leagueOptLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_leagueOptLabel.gridx = 1;
@@ -28,6 +33,7 @@ public class AdminPanel extends JPanel {
 		add(leagueOptLabel, gbc_leagueOptLabel);
 		
 		JLabel matchOptLabel = new JLabel("Match Options:");
+		matchOptLabel.setFont(labelFont);
 		GridBagConstraints gbc_matchOptLabel = new GridBagConstraints();
 		gbc_matchOptLabel.insets = labelFieldInsets;
 		gbc_matchOptLabel.gridx = 3;
@@ -35,6 +41,7 @@ public class AdminPanel extends JPanel {
 		add(matchOptLabel, gbc_matchOptLabel);
 		
 		JButton leagueButton = new JButton("League");
+		leagueButton.setFont(labelFont);
 		GridBagConstraints gbc_leagueButton = new GridBagConstraints();
 		gbc_leagueButton.insets = labelFieldInsets;
 		gbc_leagueButton.gridx = 1;
@@ -42,6 +49,7 @@ public class AdminPanel extends JPanel {
 		add(leagueButton, gbc_leagueButton);
 		
 		JButton genFixturesButton = new JButton("Generate Fixtures");
+		genFixturesButton.setFont(labelFont);
 		GridBagConstraints gbc_genFixturesButton = new GridBagConstraints();
 		gbc_genFixturesButton.insets = labelFieldInsets;
 		gbc_genFixturesButton.gridx = 3;
@@ -49,6 +57,7 @@ public class AdminPanel extends JPanel {
 		add(genFixturesButton, gbc_genFixturesButton);
 		
 		JButton seasonButton = new JButton("Season");
+		seasonButton.setFont(labelFont);
 		GridBagConstraints gbc_seasonButton = new GridBagConstraints();
 		gbc_seasonButton.insets = labelFieldInsets;
 		gbc_seasonButton.gridx = 1;
@@ -56,6 +65,7 @@ public class AdminPanel extends JPanel {
 		add(seasonButton, gbc_seasonButton);
 		
 		JButton matchRefsButton = new JButton("Assign Match Referees");
+		matchRefsButton.setFont(labelFont);
 		GridBagConstraints gbc_matchRefsButton = new GridBagConstraints();
 		gbc_matchRefsButton.insets = labelFieldInsets;
 		gbc_matchRefsButton.gridx = 3;
@@ -63,6 +73,7 @@ public class AdminPanel extends JPanel {
 		add(matchRefsButton, gbc_matchRefsButton);
 		
 		JButton teamButton = new JButton("Team");
+		teamButton.setFont(labelFont);
 		GridBagConstraints gbc_teamButton = new GridBagConstraints();
 		gbc_teamButton.insets = labelFieldInsets;
 		gbc_teamButton.gridx = 1;
@@ -70,6 +81,7 @@ public class AdminPanel extends JPanel {
 		add(teamButton, gbc_teamButton);
 		
 		JButton recMatchButton = new JButton("Record Matches");
+		recMatchButton.setFont(labelFont);
 		GridBagConstraints gbc_recMatchButton = new GridBagConstraints();
 		gbc_recMatchButton.insets = labelFieldInsets;
 		gbc_recMatchButton.gridx = 3;
@@ -77,6 +89,7 @@ public class AdminPanel extends JPanel {
 		add(recMatchButton, gbc_recMatchButton);
 		
 		JButton managersButton = new JButton("Managers");
+		managersButton.setFont(labelFont);
 		GridBagConstraints gbc_managersButton = new GridBagConstraints();
 		gbc_managersButton.insets = labelFieldInsets;
 		gbc_managersButton.gridx = 1;
@@ -84,6 +97,7 @@ public class AdminPanel extends JPanel {
 		add(managersButton, gbc_managersButton);
 		
 		JButton updateLeagueDataButton = new JButton("Update League Data");
+		updateLeagueDataButton.setFont(labelFont);
 		GridBagConstraints gbc_updateLeagueDataButton = new GridBagConstraints();
 		gbc_updateLeagueDataButton.insets = labelFieldInsets;
 		gbc_updateLeagueDataButton.gridx = 3;
@@ -91,6 +105,7 @@ public class AdminPanel extends JPanel {
 		add(updateLeagueDataButton, gbc_updateLeagueDataButton);
 		
 		JButton refereesButton = new JButton("Referees");
+		refereesButton.setFont(labelFont);
 		GridBagConstraints gbc_refereesButton = new GridBagConstraints();
 		gbc_refereesButton.insets = labelFieldInsets;
 		gbc_refereesButton.gridx = 1;
@@ -98,6 +113,7 @@ public class AdminPanel extends JPanel {
 		add(refereesButton, gbc_refereesButton);
 		
 		JButton playersButton = new JButton("Players");
+		playersButton.setFont(labelFont);
 		GridBagConstraints gbc_playersButton = new GridBagConstraints();
 		gbc_playersButton.insets = labelFieldInsets;
 		gbc_playersButton.gridx = 1;
